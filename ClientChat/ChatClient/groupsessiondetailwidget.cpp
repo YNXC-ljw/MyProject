eea5396b32aea73c19c4c6c47db2f1aca02b1b43
pengge-student-c-language <2108088572@qq.com>
2026-09-15 21:02:29 +0800

    即时通讯系统

--- ClientChat/ChatClient/groupsessiondetailwidget.cpp
+++ ClientChat/ChatClient/groupsessiondetailwidget.cpp
@@ -19,8 +19,9 @@ GroupSessionDetailWidget::GroupSessionDetailWidget(QWidget* parent) :QDialog(par
 
     // 2.创建布局管理器
     QVBoxLayout* vlayout = new QVBoxLayout();
-    vlayout->setSpacing(0);
-    vlayout->setContentsMargins(0,0,0,0);
+    vlayout->setSpacing(10);
+    vlayout->setContentsMargins(50,20,50,50);
+    vlayout->setAlignment(Qt::AlignTop);
     this->setLayout(vlayout);
 
     // 3.创建滚动区域
@@ -31,29 +32,35 @@ GroupSessionDetailWidget::GroupSessionDetailWidget(QWidget* parent) :QDialog(par
     scrollArea->horizontalScrollBar()->setStyleSheet("QScrollBar:horizontal { height: 0px; }");
     scrollArea->setFixedSize(310,350);
     scrollArea->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    scrollArea->setStyleSheet("QWidget { background-color: transparent; border: none; }");
 
     // 3.2 创建QWidget对象
     QWidget* container = new QWidget();
     scrollArea->setWidget(container);
 
     // 3.3 给container里面再添加一个网格布局
-    QGridLayout* glayout = new QGridLayout();
+    glayout = new QGridLayout();
     glayout->setContentsMargins(0,0,0,0);
-    glayout->setSpacing(0);
+    glayout->setSpacing(12);
     glayout->setAlignment(Qt::AlignTop | Qt::AlignLeft); // 控件都按左上对齐
     container->setLayout(glayout);
 
-    // 3.4 把布局管理器添加到滚动区域
+    // 3.4 把滚动区域添加到垂直布局管理器
     vlayout->addWidget(scrollArea);
 
     // 4. 添加 "添加按钮"
     AvatarItem* addBtn = new AvatarItem(QIcon(":/resource/image/cross.png"), "添加");
+    addBtn->setMinimumSize(0, 0);          // 先取消原来的最小尺寸限制
+    addBtn->setFixedSize(70, 85);          // 设置适合当前网格的大小（可自己调）
     glayout->addWidget(addBtn, 0, 0);
 
     // 5. 添加 "群聊名称"
     QLabel* groupNameTag = new QLabel();
     groupNameTag->setText("群聊名称");
-    groupNameTag->setStyleSheet("QLabel {font-weight: 700;}");
+    groupNameTag->setFixedHeight(50);
+    groupNameTag->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Expanding);
+    groupNameTag->setStyleSheet("QLabel {font-weight: 700; font-size: 16px; }");
+    groupNameTag->setAlignment(Qt::AlignBottom);
     vlayout->addWidget(groupNameTag);
 
     // 6. 添加 真实的群聊名字 和 修改按钮
@@ -65,15 +72,60 @@ GroupSessionDetailWidget::GroupSessionDetailWidget(QWidget* parent) :QDialog(par
 
     // 6.2 创建真实群聊名字的 label
     QLabel* groupNameLabel = new QLabel();
-    groupNameTag->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Expanding);
-    hlayout->addWidget(groupNameLabel);
+    groupNameLabel->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Expanding);
+    groupNameLabel->setFixedHeight(50);
+    groupNameLabel->setStyleSheet("QLabel { font-size: 18px; }");
+    hlayout->addWidget(groupNameLabel,0,Qt::AlignLeft | Qt::AlignVCenter);
 
+    // 6.3 创建 "修改按钮"
+    QPushButton* modifyBtn = new QPushButton();
+    modifyBtn->setFixedSize(30, 30);
+    modifyBtn->setIconSize(QSize(20, 20));
+    modifyBtn->setIcon(QIcon(":/resource/image/modify.png"));
+    modifyBtn->setStyleSheet("QPushButton { border: none; background-color: transparent; } QPushButton:pressed { background-color: rgb(230,230,230);");
+    hlayout->addWidget(modifyBtn,0,Qt::AlignRight | Qt::AlignVCenter);
+
+    // 7. 退出群聊按钮
+    QPushButton* exitGroupBtn = new QPushButton();
+    exitGroupBtn->setText("退出群聊");
+    exitGroupBtn->setFixedHeight(50);
+    exitGroupBtn->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    QString btnStyle = "QPushButton { border: 1px solid rgb(90, 90, 90); border-radius: 5px; background-color: transparent;}";
+    btnStyle += "QPushButton:pressed { background-color: rgb(230, 230, 230); }";
+    exitGroupBtn->setStyleSheet(btnStyle);
+    vlayout->addWidget(exitGroupBtn);
 
 
 // 此处构造假的数据用来测试界面
 #if TEST_UI
     groupNameLabel->setText("人类吃喝行为研究小组");
+    QIcon icon(":/resource/image/avatar.jpg");
+    for(int i = 0;i < 30;i++)
+    {
+        AvatarItem* item = new AvatarItem(icon,"张三" + QString::number(i));
+        addMenber(item);
+    }
+
 #endif
+
+}
+
+// 添加每一个"群成员"的函数
+void GroupSessionDetailWidget::addMenber(AvatarItem *avatarItem)
+{
+    avatarItem->setMinimumSize(0, 0);          // 先取消原来的最小尺寸限制
+    avatarItem->setFixedSize(70, 85);          // 设置适合当前网格的大小（可自己调）
+
+    // 拿到滚动区域中的布局管理器，取到成员变量即可
+    const int MAX_COL = 4;
+    if(curCol >= MAX_COL)
+    {
+        // 换行
+        ++curRow;
+        curCol = 0;
+    }
+    glayout->addWidget(avatarItem,curRow,curCol);
+    ++curCol;
 
 }
 
--- ClientChat/ChatClient/groupsessiondetailwidget.h
+++ ClientChat/ChatClient/groupsessiondetailwidget.h
@@ -3,12 +3,24 @@
 
 #include <QDialog>
 #include <QWidget>
+#include <QGridLayout>
+
+class AvatarItem;
 
 class GroupSessionDetailWidget : public QDialog
 {
     Q_OBJECT
 public:
     GroupSessionDetailWidget(QWidget* parent);
+
+    void addMenber(AvatarItem* avatarItem);
+private:
+    QGridLayout* glayout;
+
+    // 表示当前要添加的 AvatarItem 所处的行和列
+    // 由于整个界面上存在 "+" 这个按钮，占据了 (0, 0) 这个位置，所以第一个 AvatarItem 就要从 (0, 1) 开始了
+    int curCol = 1;
+    int curRow = 0;
 };
 
 #endif // GROUPSESSIONDETAILWIDGET_H
